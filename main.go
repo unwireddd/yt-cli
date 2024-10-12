e08eb5d4ab91c2637ef9b9641929d389a4e77e3d
unwired <unwired@tutanota.de>
2024-10-12 23:17:58 +0000

    Update main.go

--- main.go
+++ main.go
@@ -40,6 +40,9 @@ func main() {
 
 	items = append(items, item("Search"))
 	items = append(items, item("Test"))
+	items = append(items, item("Add"))
+	items = append(items, item("Add / Remove a channel"))
+	items = append(items, item("History"))
 
 	const defaultWidth = 20
 
@@ -58,34 +61,41 @@ func main() {
 		os.Exit(1)
 	}
 
-	ator, _ := soup.Get(link)
-	atorvid := soup.HTMLParse(ator)
-	owtput := atorvid.Find("div", "class", "pure-g").HTML()
-	re := regexp.MustCompile(`<a[^>]*>.*?<p dir="auto">.*?</p>.*?</a>`)
-	matches := re.FindAllString(owtput, -1)
-	for _, match := range matches {
+	// tutaj trzeba zrobic if statement ze jak isHistory jest na false to sie robi to a jak nie to inaczej
+	if !isHistory {
+		ator, _ := soup.Get(link)
+		atorvid := soup.HTMLParse(ator)
+		owtput := atorvid.Find("div", "class", "pure-g").HTML()
+		re := regexp.MustCompile(`<a[^>]*>.*?<p dir="auto">.*?</p>.*?</a>`)
+		matches := re.FindAllString(owtput, -1)
+		for _, match := range matches {
 
-		match = strings.Replace(match, "<a href=", "", -1)
-		if video < 10 {
-			match = strings.Replace(match, `><p dir="auto">`, "			", -1)
-		} else {
-			match = strings.Replace(match, `><p dir="auto">`, "       ", -1)
+			match = strings.Replace(match, "<a href=", "", -1)
+			if video < 10 {
+				match = strings.Replace(match, `><p dir="auto">`, "			", -1)
+			} else {
+				match = strings.Replace(match, `><p dir="auto">`, "       ", -1)
+			}
+			//match = strings.Replace(match, `><p dir="auto">`, "			", -1)
+			match = strings.Replace(match, `</p></a>`, "", -1)
+			match = strings.Replace(match, `"`, "", -1)
+			match = strings.Replace(match, `/`, fmt.Sprintf("https://www.youtube.com/"), -1)
+			cutter := regexp.MustCompile(`https?://[^\s]+`)
+			link := cutter.FindString(match)
+			match = strings.Replace(match, `https://www.youtube.com/`, fmt.Sprintf(""), -1)
+			match = strings.Replace(match, "watch?v=", "", 1)
+			match = strings.Replace(match, "?list=", "[Playlist]", 1)
+			match = removeFirstAlphanumeric(match)
+			match = strings.TrimSpace(match)
+
+			mecze = append(mecze, match)
+
+			videos[match] = link
+			video = video + 1
 		}
-		//match = strings.Replace(match, `><p dir="auto">`, "			", -1)
-		match = strings.Replace(match, `</p></a>`, "", -1)
-		match = strings.Replace(match, `"`, "", -1)
-		match = strings.Replace(match, `/`, fmt.Sprintf("https://www.youtube.com/"), -1)
-		cutter := regexp.MustCompile(`https?://[^\s]+`)
-		link := cutter.FindString(match)
-		match = strings.Replace(match, `https://www.youtube.com/`, fmt.Sprintf(""), -1)
-		match = strings.Replace(match, "watch?v=", "", 1)
-		match = removeFirstAlphanumeric(match)
-		match = strings.TrimSpace(match)
-
-		mecze = append(mecze, match)
-
-		videos[match] = link
-		video = video + 1
+	} else {
+		// notatka ze przeciez mam jeszcze ta mape co przy robieniu history sie zrobila
+		videos = titleLinkMap
 	}
 
 	for i, str := range mecze {
@@ -98,20 +108,36 @@ func main() {
 		itemki = append(itemki, mecze[i])
 	}
 x:
+	if isHistory {
+		l = list.New(itemshist, itemDelegate{}, defaultWidth, listHeight)
+		l.Title = "Select the video you'd like to watch"
+		l.SetShowStatusBar(false)
+		l.SetFilteringEnabled(false)
+		l.Styles.Title = titleStyle
+		l.Styles.PaginationStyle = paginationStyle
+		l.Styles.HelpStyle = helpStyle
 
-	l = list.New(itemstwo, itemDelegate{}, defaultWidth, listHeight)
-	l.Title = "Select the video you'd like to watch"
-	l.SetShowStatusBar(false)
-	l.SetFilteringEnabled(false)
-	l.Styles.Title = titleStyle
-	l.Styles.PaginationStyle = paginationStyle
-	l.Styles.HelpStyle = helpStyle
+		m = modeltwo{list: l}
 
-	m = modeltwo{list: l}
+		if _, err := tea.NewProgram(m.(tea.Model)).Run(); err != nil {
+			fmt.Println("Error running program:", err)
+			os.Exit(1)
+		}
+	} else {
+		l = list.New(itemstwo, itemDelegate{}, defaultWidth, listHeight)
+		l.Title = "Select the video you'd like to watch"
+		l.SetShowStatusBar(false)
+		l.SetFilteringEnabled(false)
+		l.Styles.Title = titleStyle
+		l.Styles.PaginationStyle = paginationStyle
+		l.Styles.HelpStyle = helpStyle
 
-	if _, err := tea.NewProgram(m.(tea.Model)).Run(); err != nil {
-		fmt.Println("Error running program:", err)
-		os.Exit(1)
+		m = modeltwo{list: l}
+
+		if _, err := tea.NewProgram(m.(tea.Model)).Run(); err != nil {
+			fmt.Println("Error running program:", err)
+			os.Exit(1)
+		}
 	}
 
 	l = list.New(itemsthree, itemDelegate{}, defaultWidth, listHeight)
